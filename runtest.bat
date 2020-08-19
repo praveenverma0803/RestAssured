f8e97ea51124c3484322832e70e5fbd66c400774
prveenverma0803 <praeenverma0803@gmail.com>
2020-08-20 01:18:21 +0530

    configured allure report

---
+++ runtest.bat
@@ -0,0 +1,4 @@
+D:
+cd D:\WORKSPACE\Selenium_Project\RestAssuredAPI
+call mvn test
+allure serve target/allure-results
--- src/test/java/apipackage/TC006_Authorization.java
+++ src/test/java/apipackage/TC006_Authorization.java
@@ -22,8 +22,10 @@ public class TC006_Authorization extends TestBase {
 		//authschema.setAccessToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzY2F0ZWNoLmNvbSIsImlhdCI6MTU5Nzc1MTk3MiwiZXhwIjoxNTk3ODM4MzcyLCJqdGkiOiI2YmEyYzJmZS1iYzM2LTRiZTQtOGMyYi1kNWIwZDU0NTFlNzgiLCJzdWIiOiJzY2FwbGFubmVyIn0.-gb-CxM5wqnhxLMktsLqDhzNgWInVNZNRL-vEQqp6Ygaf259CDQxBlDEIayt98ib4SrbOuRE0UpRhTNXiSEAzw");
 				
 		//RestAssured.authentication = authschema;
-		String Key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzY2F0ZWNoLmNvbSIsImlhdCI6MTU5Nzc1MTk3MiwiZXhwIjoxNTk3ODM4MzcyLCJqdGkiOiI2YmEyYzJmZS1iYzM2LTRiZTQtOGMyYi1kNWIwZDU0NTFlNzgiLCJzdWIiOiJzY2FwbGFubmVyIn0.-gb-CxM5wqnhxLMktsLqDhzNgWInVNZNRL-vEQqp6Ygaf259CDQxBlDEIayt98ib4SrbOuRE0UpRhTNXiSEAzw";
-//Request Object
+		String Key = 
+				"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJzY2F0ZWNoLmNvbSIsImlhdCI6MTU5Nzg2MjU1OSwiZXhwIjoxNTk3OTQ4OTU5LCJqdGkiOiIxY2UyMmNlNC1iMDU1LTRhNGItYWE3NS1iZWJmMDM0YzU4NzgiLCJzdWIiOiJzY2FwbGFubmVyIn0.4kvf597TXo_RONG2WHnrsAqEbb5J44sLFmpS-Y_KsxCE2_iINvlIR2qih1_LsIixCcMuYUyluBsAnjsIN96xJg";
+		
+		//Request Object
 		RequestSpecification  httpRequest = RestAssured.given();	
 		httpRequest.queryParam("today=11%2F06%2F2021");	
 		httpRequest.auth().oauth2(Key);
