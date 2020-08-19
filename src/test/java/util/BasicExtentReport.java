3c6d7945b0215cdcec0b6fbd3cbab27bb9933e42
prveenverma0803 <praeenverma0803@gmail.com>
2020-08-19 23:00:09 +0530

    extent report configured

--- src/test/java/util/BasicExtentReport.java
+++ src/test/java/util/BasicExtentReport.java
@@ -12,7 +12,6 @@ import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class BasicExtentReport extends TestListenerAdapter 
 {
-
 	//builds a new report using the html template 
     ExtentHtmlReporter htmlReporter;
     
@@ -20,42 +19,48 @@ public class BasicExtentReport extends TestListenerAdapter
     //helps to generate the logs in test report.
     ExtentTest test;
    
-    public void startReport(ITestContext testContext ) {
+    public void onStart(ITestContext testcontext) {
     	// initialize the HtmlReporter
         htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") +"/BasicExtentReport/testReport.html");
 
         
         //configuration items to change the look and feel
         //add content, manage tests etc
-        htmlReporter.config().setDocumentTitle("Extent Report Demo");
-        htmlReporter.config().setReportName("Test Report");
-        htmlReporter.config().setTheme(Theme.STANDARD);
-        htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+        htmlReporter.config().setDocumentTitle("Extent API Report");
+        htmlReporter.config().setReportName("API results");
+        htmlReporter.config().setTheme(Theme.DARK);
+        //htmlReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
         
         //initialize ExtentReports and attach the HtmlReporter
         extent = new ExtentReports();
         extent.attachReporter(htmlReporter);
+        extent.setSystemInfo("Project Name", "Users Dashboard");;
          
     }
-     
- 
-    public void getResult(ITestResult result) {
-        if(result.getStatus() == ITestResult.FAILURE) {
-            test.log(Status.FAIL, MarkupHelper.createLabel(result.getName()+" FAILED ", ExtentColor.RED));
-            test.fail(result.getThrowable());
-        }
-        else if(result.getStatus() == ITestResult.SUCCESS) {
-            test.log(Status.PASS, MarkupHelper.createLabel(result.getName()+" PASSED ", ExtentColor.GREEN));
-        }
-        else {
-            test.log(Status.SKIP, MarkupHelper.createLabel(result.getName()+" SKIPPED ", ExtentColor.ORANGE));
-            test.skip(result.getThrowable());
-        }
+    
+    public void onTestSuccess(ITestResult result) {
+    		
+    	test = extent.createTest(result.getName());
+    	test.log(Status.PASS,"Testcase is" +  MarkupHelper.createLabel(result.getName() +" PASSED ", ExtentColor.GREEN));
     }
-     
 
-    public void tearDown() {
+    public void onTestFailure(ITestResult result) {
+		
+    	test = extent.createTest(result.getName());
+    	test.log(Status.FAIL,"Testcase is" +  MarkupHelper.createLabel(result.getName() +" FAILED ", ExtentColor.RED));
+    	test.log(Status.FAIL,"Testcase is" +result.getThrowable());
+    }
+
+public void onTestSkip(ITestResult result) {
+		
+    	test = extent.createTest(result.getName());
+    	test.log(Status.SKIP,"Testcase is" + MarkupHelper.createLabel(result.getName() +" SKIPPED ", ExtentColor.ORANGE));
+    	test.log(Status.SKIP,"Testcase is" +result.getThrowable());
+    }
+    
+    public void onFinish(ITestContext testcontext) {
     	//to write or update test information to reporter
         extent.flush();
+        
     }
 }
